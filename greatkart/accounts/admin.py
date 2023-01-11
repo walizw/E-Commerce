92beb43e98a7974cce303df53fd3bb606acd9de9
Sara <yojan.bustamante@udea.edu.co>
2023-01-11 17:30:36 +0000

    the password field is now readonly

--- greatkart/accounts/admin.py
+++ greatkart/accounts/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import Account
 
-admin.site.register (Account)
+class AccountAdmin (UserAdmin):
+    list_display = (
+        "email",
+        "first_name",
+        "last_name",
+        "username",
+        "date_joined",
+        "last_login",
+        "is_active",
+    )
+    list_display_links = ("email", "first_name", "last_name", "username")
+
+    readonly_fields = ("last_login", "date_joined")
+    ordering = ("-date_joined",)
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register (Account, AccountAdmin)
